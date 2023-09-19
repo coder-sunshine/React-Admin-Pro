ad0e22be10380a2713f45839bc27fa913b636d88
wff <283953455@qq.com>
2023-09-19 16:25:17 +0800

    feat: 🚀 给页面加过渡动画

--- src/layouts/components/Main/index.tsx
+++ src/layouts/components/Main/index.tsx
@@ -1,22 +1,30 @@
 import { Layout } from 'antd'
-import { useOutlet } from 'react-router-dom'
+import { useLocation, useOutlet } from 'react-router-dom'
 import LayoutTabs from '../Tabs'
-import { useContext, useEffect } from 'react'
+import { createRef, useContext, useEffect } from 'react'
 import { RefreshContext } from '@/context/Refresh'
 import Maximize from './components/Maximize'
 import { RootState, useDispatch, useSelector } from '@/redux'
 import { useDebounceFn } from 'ahooks'
 import { setGlobalState } from '@/redux/modules/global'
+import { CSSTransition, SwitchTransition } from 'react-transition-group'
+import { RouteObjectType } from '@/routers/interface'
 
 import './index.less'
+
+type RouteTypeWithNodeRef = {
+  nodeRef?: React.Ref<HTMLElement> | undefined
+} & RouteObjectType
 
 const { Content } = Layout
 
 const LayoutMain: React.FC = () => {
   const dispatch = useDispatch()
+  const { pathname } = useLocation()
   const outlet = useOutlet()
   const { outletShow } = useContext(RefreshContext)
   const { maximize, isCollapse } = useSelector((state: RootState) => state.global)
+  const { flatMenuList } = useSelector((state: RootState) => state.auth)
 
   // 监听窗口变化，动态设置菜单栏关闭与否
   const { run } = useDebounceFn(
@@ -38,11 +46,20 @@ const LayoutMain: React.FC = () => {
     root.classList.toggle('main-maximize', maximize)
   }, [maximize])
 
+  // Solve the transition animation that causes useEffect to execute multiple times
+  // @see: http://reactcommunity.org/react-transition-group/with-react-router
+  const menuList: RouteTypeWithNodeRef[] = flatMenuList.map(item => ({ ...item, nodeRef: createRef() }))
+  const { nodeRef } = menuList.find(route => route.path === pathname) ?? {}
+
   return (
     <>
       <Maximize />
       <LayoutTabs />
-      <Content>{outletShow && outlet}</Content>
+      <SwitchTransition>
+        <CSSTransition classNames='fade' key={pathname} nodeRef={nodeRef} timeout={300} exit={false} unmountOnExit>
+          <Content ref={nodeRef}>{outletShow && outlet}</Content>
+        </CSSTransition>
+      </SwitchTransition>
     </>
   )
 }
