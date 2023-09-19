606c1bb058ce55cbd5bcdaff7df7d98f7746b092
wff <283953455@qq.com>
2023-09-19 16:10:53 +0800

    feat: 🚀 tabs页面最大化功能按钮

---
+++ src/layouts/components/Main/components/Maximize.tsx
@@ -0,0 +1,20 @@
+import { RootState, useDispatch, useSelector } from '@/redux'
+import { setGlobalState } from '@/redux/modules/global'
+
+const Maximize: React.FC = () => {
+  const dispatch = useDispatch()
+
+  const { maximize } = useSelector((state: RootState) => state.global)
+
+  return (
+    <>
+      {maximize && (
+        <div className='maximize-icon' onClick={() => dispatch(setGlobalState({ key: 'maximize', value: false }))}>
+          <i className='iconfont icon-tuichu'></i>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Maximize
--- src/layouts/components/Main/index.tsx
+++ src/layouts/components/Main/index.tsx
@@ -1,16 +1,29 @@
 import { Layout } from 'antd'
 import { useOutlet } from 'react-router-dom'
 import LayoutTabs from '../Tabs'
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { RefreshContext } from '@/context/Refresh'
+import Maximize from './components/Maximize'
+import { RootState, useSelector } from '@/redux'
+
+import './index.less'
 
 const { Content } = Layout
 
 const LayoutMain: React.FC = () => {
   const outlet = useOutlet()
   const { outletShow } = useContext(RefreshContext)
+  const { maximize } = useSelector((state: RootState) => state.global)
+
+  // 监控当前页面是否最大化，动态添加类
+  useEffect(() => {
+    const root = document.getElementById('root') as HTMLElement
+    root.classList.toggle('main-maximize', maximize)
+  }, [maximize])
+
   return (
     <>
+      <Maximize />
       <LayoutTabs />
       <Content>{outletShow && outlet}</Content>
     </>
