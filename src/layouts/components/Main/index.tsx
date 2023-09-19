4f09e47a3e5f43322bf7308ccfa3e0aa9110aa13
wff <283953455@qq.com>
2023-09-19 16:16:41 +0800

    feat: 🚀 监听窗口变化,动态更改菜单开关

--- src/layouts/components/Main/index.tsx
+++ src/layouts/components/Main/index.tsx
@@ -4,16 +4,33 @@ import LayoutTabs from '../Tabs'
 import { useContext, useEffect } from 'react'
 import { RefreshContext } from '@/context/Refresh'
 import Maximize from './components/Maximize'
-import { RootState, useSelector } from '@/redux'
+import { RootState, useDispatch, useSelector } from '@/redux'
+import { useDebounceFn } from 'ahooks'
+import { setGlobalState } from '@/redux/modules/global'
 
 import './index.less'
 
 const { Content } = Layout
 
 const LayoutMain: React.FC = () => {
+  const dispatch = useDispatch()
   const outlet = useOutlet()
   const { outletShow } = useContext(RefreshContext)
-  const { maximize } = useSelector((state: RootState) => state.global)
+  const { maximize, isCollapse } = useSelector((state: RootState) => state.global)
+
+  // 监听窗口变化，动态设置菜单栏关闭与否
+  const { run } = useDebounceFn(
+    () => {
+      const screenWidth = document.body.clientWidth
+      const shouldCollapse = screenWidth < 1200
+      if (isCollapse !== shouldCollapse) dispatch(setGlobalState({ key: 'isCollapse', value: shouldCollapse }))
+    },
+    { wait: 100 }
+  )
+  useEffect(() => {
+    window.addEventListener('resize', run, false)
+    return () => window.removeEventListener('resize', run)
+  }, [])
 
   // 监控当前页面是否最大化，动态添加类
   useEffect(() => {
