1095c6245ee0b40d8d15174d935acf7cc29f222a
wff <283953455@qq.com>
2023-10-15 11:19:00 +0800

    feat: 🚀 layout --> 经典布局

--- src/layouts/LayoutClassic/index.tsx
+++ src/layouts/LayoutClassic/index.tsx
@@ -1,33 +1,41 @@
 import { RootState, useSelector } from '@/redux'
 import { Layout } from 'antd'
-import LayoutMenu from '../components/Menu'
-
+import ToolBarLeft from '@/layouts/components/Header/ToolBarLeft'
+import ToolBarRight from '@/layouts/components/Header/ToolBarRight'
+import LayoutMenu from '@/layouts/components/Menu'
+import LayoutMain from '@/layouts/components/Main'
 import logo from '@/assets/images/logo.svg'
-import { getFirstLevelMenuList } from '@/utils'
 
 import './index.less'
-
-const { Header } = Layout
+const { Header, Sider } = Layout
 const APP_TITLE = import.meta.env.VITE_GLOB_APP_TITLE
 
 const LayoutClassic: React.FC = () => {
-  const { menuSplit } = useSelector((state: RootState) => state.global)
-  const { showMenuList } = useSelector((state: RootState) => state.auth)
-
+  const { isCollapse } = useSelector((state: RootState) => state.global)
   // 获取所有第一级菜单 --> 用于拆分菜单
-  const firstLevelMenuList = getFirstLevelMenuList(showMenuList)
 
   return (
     <section className='layout-classic'>
       <Header>
-        <div className={`header-lf ${menuSplit ? 'hide-logo' : 'mask-image'}`}>
+        <div className='header-lf mask-image'>
           <div className='logo'>
             <img src={logo} alt='logo' className='logo-img' />
             <h2 className='logo-text'>{APP_TITLE}</h2>
           </div>
-          {menuSplit ? <LayoutMenu mode='horizontal' menuList={firstLevelMenuList} /> : null}
+          {<ToolBarLeft />}
+        </div>
+        <div className='header-ri'>
+          <ToolBarRight />
         </div>
       </Header>
+      <div className='classic-content'>
+        <Sider width={210} collapsed={isCollapse}>
+          <LayoutMenu mode='inline' />
+        </Sider>
+        <div className='classic-main'>
+          <LayoutMain />
+        </div>
+      </div>
     </section>
   )
 }
