ca078dc7acc21922aadb5357dd954d0c2b22829c
wff <283953455@qq.com>
2023-11-16 14:13:01 +0800

    feat: 🚀 常用组件 -> 取色器

--- src/typings/window.d.ts
+++ src/typings/window.d.ts
@@ -3,6 +3,9 @@ import { NavigateFunction } from 'react-router-dom'
 declare global {
   interface Window {
     $navigate: NavigateFunction
+    EyeDropper: new () => {
+      open(options?: { signal: AbortSignal }): Promise<{ sRGBHex: string }>
+    }
   }
 
   interface Navigator {
