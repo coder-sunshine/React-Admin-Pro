af8a3a9656fb748b81cd23a4005fff9496a04851
wff <283953455@qq.com>
2024-02-17 15:36:54 +0800

    fix: 🧩 fix rtk error report

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,9 +1,8 @@
 import { TypedUseSelectorHook, useDispatch as useReduxDispatch, useSelector as useReduxSelector } from 'react-redux'
-import { configureStore, combineReducers, Middleware, AnyAction, ThunkDispatch } from '@reduxjs/toolkit'
-
+import { configureStore, combineReducers, Middleware } from '@reduxjs/toolkit'
 import { persistStore, persistReducer } from 'redux-persist'
+import { thunk } from 'redux-thunk'
 import storage from 'redux-persist/lib/storage'
-import reduxThunk from 'redux-thunk'
 import user from './modules/user'
 import global from './modules/global'
 import auth from './modules/auth'
@@ -26,12 +25,12 @@ const persistConfig = {
 const persistReducerConfig = persistReducer(persistConfig, reducer)
 
 // redux middleWares(self configuration)
-const middleWares: Middleware[] = [reduxThunk]
+const middleWares: Middleware[] = [thunk]
 
 // store
 export const store = configureStore({
   reducer: persistReducerConfig,
-  middleware: middleWares,
+  middleware: getDefaultMiddleware => getDefaultMiddleware({ serializableCheck: false }).concat(middleWares),
   devTools: true,
 })
 
@@ -40,6 +39,6 @@ export const persistor = persistStore(store)
 
 // redux hooks
 export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = ThunkDispatch<RootState, any, AnyAction>
+export type AppDispatch = typeof store.dispatch
 export const useSelector: TypedUseSelectorHook<RootState> = useReduxSelector
 export const useDispatch = () => useReduxDispatch<AppDispatch>()
